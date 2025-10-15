f44a7deabc649f3411f8f57e393b6f7f3366f247
rengoku <gamerelbistanli@gmail.com>
2025-10-15 07:05:17 +0300

    güncelleme
    v

--- odevislem/src/Main.java
+++ odevislem/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 public static void main(String[] args) {
-    }
+    int bolme1 = 6;
+    int bolme2 = 2;
+    int sonucbolme = bolme1 / bolme2;
+    System.out.println(sonucbolme);
+
+    int carpma1 = 2;
+            int carpma2 = 5;
+                    int sonuccarpma = carpma1 * carpma2;
+                    System.out.println(sonuccarpma);
+
+                    int cikarma1 = 10;
+                            int cikarma2 = 6;
+                                    int sonuccikarma = cikarma1 - cikarma2;
+                    System.out.println(sonuccikarma);
+
+
+                    int toplama1 = 5;
+                    int toplama2 = 6;
+                    int sonuctoplama = toplama1 + toplama2;
+                    System.out.println(sonuctoplama);
+}
 }
 
 
--- odevucgen/src/Main.java
+++ odevucgen/src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int taban = 5;
+                int yukseklik = 10;
+                int alan = (taban * yukseklik)/2;
+                System.out.println(alan);
+
+
+
+                int kenar = 8;
+                        int cevre = kenar * 3;
+                        System.out.println(cevre);
     }
 }
