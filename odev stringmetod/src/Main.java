698f7d07dd1c4cdaecaf81fa54affa53f37f68b8
rengoku <gamerelbistanli@gmail.com>
2025-10-19 17:37:19 +0300

    d

--- odev stringmetod/src/Main.java
+++ odev stringmetod/src/Main.java
@@ -1,10 +1,8 @@
 void main(){
     String odev = "Merhaba dünya";
-IO.print(odev.length()+" "); //cümledeki karakter sayısı
-IO.print(odev.charAt(12)+" ");//cümledeki 0 dan başlayarak hangi sayıyı söylersen denk gelen karakteri yazar
-IO.print(odev.toUpperCase()+" ");//cümleyi full büyük yazar
-IO.print(odev.toLowerCase()+" ");//cümleyi full küçük yazar
-IO.print(odev+" ");//cümleyi yazar
-// abi sondaki yerleri bilerek güzel yazsın diye ürettim yoksa onların gerekmeyeceğini biliyom
-
+IO.println(odev.length()); //cümledeki karakter sayısı
+IO.println(odev.charAt(12));//cümledeki 0 dan başlayarak hangi sayıyı söylersen denk gelen karakteri yazar
+IO.println(odev.toUpperCase());//cümleyi full büyük yazar
+IO.println(odev.toLowerCase());//cümleyi full küçük yazar
+IO.println(odev);//cümleyi yazar
 }
