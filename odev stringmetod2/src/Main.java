419595bcbeba51e816f413f56c3e75f6c55b1bea
rengoku <gamerelbistanli@gmail.com>
2025-10-21 20:04:33 +0300

    a

---
+++ odev stringmetod2/src/Main.java
@@ -0,0 +1,10 @@
+void main(){
+    String karaktersy=("1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+int kelimesayi =(karaktersy.length());
+if (karaktersy.length()<100) {
+    IO.print("kelime sayın:"+kelimesayi+" yetersiz");}
+    else
+    IO.print("kelime sayın"+kelimesayi+" yeterli");
+
+    }
+
